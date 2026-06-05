c9bc93a1707bb57cb436da70b572cf6258818d03
hjhsamuel <hjh_samuel@163.com>
2026-06-05 17:20:22 +0800

    fix: ui

--- ui/route.go
+++ ui/route.go
@@ -1,18 +1,36 @@
 package ui
 
 import (
+	"embed"
+	"io/fs"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
+//go:embed dist/*
+var distFS embed.FS
+
 func Route(e *gin.Engine) {
-	e.StaticFile("/index.html", "ui/dist/index.html")
-	e.StaticFile("/main.js", "ui/dist/main.js")
-	e.StaticFile("/styles.css", "ui/dist/styles.css")
-	e.StaticFile("/favicon.svg", "ui/dist/favicon.svg")
-	e.StaticFile("/favicon.ico", "ui/dist/favicon.ico")
-	e.StaticFile("/icons.svg", "ui/dist/icons.svg")
-	e.Static("/assets", "ui/dist/assets")
+	staticFS, _ := fs.Sub(distFS, "dist")
+	httpFS := http.FS(staticFS)
+
+	e.StaticFileFS("/index.html", "index.html", httpFS)
+	e.StaticFileFS("/main.js", "main.js", httpFS)
+	e.StaticFileFS("/styles.css", "styles.css", httpFS)
+	e.StaticFileFS("/favicon.svg", "favicon.svg", httpFS)
+	e.StaticFileFS("/favicon.ico", "favicon.ico", httpFS)
+	e.StaticFileFS("/icons.svg", "icons.svg", httpFS)
+	e.StaticFS("/assets", httpFS)
+
 	e.NoRoute(func(ctx *gin.Context) {
-		ctx.File("ui/dist/index.html")
+		f, err := staticFS.Open("index.html")
+		if err != nil {
+			ctx.String(http.StatusNotFound, "not found")
+			return
+		}
+		defer f.Close()
+		stat, _ := f.Stat()
+		ctx.DataFromReader(http.StatusOK, stat.Size(), "text/html; charset=utf-8", f, nil)
 	})
 }
